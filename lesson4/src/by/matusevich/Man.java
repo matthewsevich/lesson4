8dee1ae658e6c49ba0db5b3867c397a425955ea5
sergeymatusevich <serg.matusevich@gmail.com>
2020-02-19 20:21:29 +0300

    new commit

--- lesson4/src/by/matusevich/Man.java
+++ lesson4/src/by/matusevich/Man.java
@@ -1,10 +1,27 @@
 package by.matusevich;
 
+import javax.xml.namespace.QName;
+
 public class Man {
+    int hairLength = 10;
+    String name;
+
     public Man(){
         System.out.println("Call Default constructor");
     }
+
+    public Man(int hairLength) {
+        this.hairLength = hairLength;
+    }
+
+    public Man(String name){
+        System.out.println("constructor with param " + name);
+        this.name = name;
+
+    }
     public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().maxMemory());
+        System.out.println(Runtime.getRuntime().totalMemory());
         System.out.println("declare variable");
         Man max;
         System.out.println("init variable");
@@ -14,6 +31,8 @@ public class Man {
         System.out.println(max2);
         max2=max;
         System.out.println(max2);
-
+        Man superman = new Man("Superman");
+        System.out.println(superman.name);
+        System.out.println(superman.hairLength);
     }
 }
