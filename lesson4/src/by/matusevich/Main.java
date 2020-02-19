8108660442e9bad59c00cf071e6003d8235e99a1
sergeymatusevich <serg.matusevich@gmail.com>
2020-02-19 19:34:43 +0300

    first commit

---
+++ lesson4/src/by/matusevich/Main.java
@@ -0,0 +1,15 @@
+package by.matusevich;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b1= 42;
+        System.out.println(Integer.toBinaryString(b1));
+        byte b2=-42;
+        System.out.println(Integer.toBinaryString(b2));
+        float f1= 1.11f;
+        double d1=1.21d;
+        float f2=f1+1.12f;
+        
+    }
+}
---
+++ lesson4/src/by/matusevich/Man.java
@@ -0,0 +1,4 @@
+package by.matusevich;
+
+public class Man {
+}
